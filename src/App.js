8f5d1286eeb47c4d63d80f13c37d2a1692ccfe90
Vinícius Souza <tradutorvinicius@gmail.com>
2023-03-06 19:07:14 -0300

    Resolve requisito 5

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import Card from './components/Card';
 import Form from './components/Form';
 
+const MIN_LENGTH = 0;
+const MAX_POINTS_EACH = 90;
+const ATTR_TOTAL = 210;
+
 class App extends React.Component {
   state = {
     name: '',
@@ -34,6 +38,14 @@ class App extends React.Component {
       trunfo,
     } = this.state;
 
+    const buttonDisabled = (name.length <= MIN_LENGTH)
+    || (description.length <= MIN_LENGTH)
+    || (imageURL.length <= MIN_LENGTH)
+    || (Number(attr1) > MAX_POINTS_EACH || Number(attr1) < MIN_LENGTH)
+    || (Number(attr2) > MAX_POINTS_EACH || Number(attr2) < MIN_LENGTH)
+    || (Number(attr3) > MAX_POINTS_EACH || Number(attr3) < MIN_LENGTH)
+    || Number(attr1) + Number(attr2) + Number(attr3) > ATTR_TOTAL;
+
     return (
       <div>
         <h1>Tryunfo</h1>
@@ -47,6 +59,7 @@ class App extends React.Component {
           cardRare={ rare }
           cardTrunfo={ trunfo }
           onInputChange={ this.onInputChange }
+          isSaveButtonDisabled={ buttonDisabled }
         />
         <Card
           cardName={ name }
